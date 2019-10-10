3b006a98a6f26c8aebea414e63db657fdccbb008
Johann Albino <johann.albino.ti@gmail.com>
2019-10-10 13:43:57 -0300

    POST funcionando

--- backend/launch_site/models.py
+++ backend/launch_site/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class LaunchSite(models.Model):
     site_id = models.CharField(max_length=100, blank=True, null=True)
     site_name = models.CharField(max_length=100, blank=True, null=True)
-    site_name_long = models.CharField(max_length=100, blank=True, null=True)
+    site_name_long = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.site_id + ' ' + self.site_name
