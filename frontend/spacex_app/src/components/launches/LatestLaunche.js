ec122d97ef11f052d869a93bd33fc233e8dabbaf
Johann Albino <johann.albino.ti@gmail.com>
2019-10-17 19:59:42 -0300

    Alterações no front

--- frontend/spacex_app/src/components/launches/LatestLaunche.js
+++ frontend/spacex_app/src/components/launches/LatestLaunche.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Container from '@material-ui/core/Container';
+
 
 export default function LatestLaunche(){
+
     
+
     return(
         <React.Fragment>
-            <div>oiiiiiis</div>
+            <Container fixed>
+                oiasdofsaiof
+            </Container>            
         </React.Fragment>
     )
 }
